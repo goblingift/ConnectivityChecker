baaade6715240b621b58af1373b6709a6522af0d
André Keßler <andre.kessler91@gmx.de>
2022-03-15 19:35:17 +0100

    Initial commit

---
+++ src/main/java/gift/goblin/ConnectivityChecker/ConnectivityCheckerApplication.java
@@ -0,0 +1,13 @@
+package gift.goblin.ConnectivityChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectivityCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectivityCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gift/goblin/ConnectivityChecker/ConnectivityCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package gift.goblin.ConnectivityChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectivityCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
